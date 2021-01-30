91b39871ce80d69b9fbf6c658b20c6d64df73c84
jrucl2d <ysk789465@gmail.com>
2021-01-30 17:13:21 +0900

    할인 정책 변경 관련 완료

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +12,22 @@ import hello.core.order.OrderServiceImpl;
 
 // 어플리케이션 실제 동작에 필요한 구현 객체를 생성하는 역할
 public class AppConfig {
+    // 중복이 제거되었고, 역할과 구현 클래스가 한 눈에 들어온다.
+    private MemberRepository nemberRepository() {
+        return new MemoryMemberRepository();
+    }
+    // 할인 정책을 변경하기 쉬워졌다. -> AppConfig(구성 영역)만 변경하고 사용 영역은 변경하지 않는다.
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(nemberRepository());
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(nemberRepository(), discountPolicy());
     }
+
 }
