a533b09f0ca1d3272f0449a6632a09853be8f13a
jrucl2d <ysk789465@gmail.com>
2021-01-31 19:21:45 +0900

    스프링 컨테이너 사용으로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 어플리케이션 실제 동작에 필요한 구현 객체를 생성하는 역할
+@Configuration
 public class AppConfig {
     // 중복이 제거되었고, 역할과 구현 클래스가 한 눈에 들어온다.
-    private MemberRepository nemberRepository() {
+    @Bean
+    public MemberRepository nemberRepository() {
         return new MemoryMemberRepository();
     }
+
     // 할인 정책을 변경하기 쉬워졌다. -> AppConfig(구성 영역)만 변경하고 사용 영역은 변경하지 않는다.
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(nemberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(nemberRepository(), discountPolicy());
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,6 +4,8 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
@@ -11,8 +13,11 @@ public class MemberApp {
         // 스프링 관련되지 않은 순수 자바 코드
 //        MemberService memberService = new MemberServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
